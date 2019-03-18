fab3a49e8267bb7cafbb18cc17fff6ca1e382f3c
kjhank <sumerian.cry@gmail.com>
2019-03-18 14:49:37 +0100

    pierwsze komponenty

--- js/app.jsx
+++ js/app.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, HashRouter, Route, Link, Switch, NavLink} from 'react-router-dom';
+import {
+  BrowserRouter,
+  HashRouter,
+  Route,
+  Link,
+  Switch,
+  NavLink
+} from 'react-router-dom';
 
 document.addEventListener('DOMContentLoaded', function() {
 
   class MainHeader extends React.Component {
     render() {
-      return <header className="main-header"><h1>{this.props.title}</h1></header>
+      return <header className="main-header">
+        <h1>{this.props.title}</h1>
+      </header>
     }
   }
 
   class SplashScreen extends React.Component {
     render() {
       return <h2>Let's get started!</h2>
+    }
+  }
+
+  class Seating extends React.Component {
+    render() {
+      return <form>
+        <label htmlFor="wideangle">
+          <input type="checkbox" id="wideangle"/>wide angle seating
+        </label>
+      </form>
     }
   }
 
@@ -24,22 +43,35 @@ document.addEventListener('DOMContentLoaded', function() {
 
   class Navigation extends React.Component {
     render() {
-      return <nav class="main-nav"><a href="/"><i className="fas fa-home nav-icon icon"></i></a></nav>
+      return <nav className="main-nav">
+        <Link to="/">
+          <i className="fas fa-home nav-icon icon"></i>
+        </Link>
+      </nav>
+    }
+  }
+
+  class FouOhFour extends React.Component {
+    render() {
+      return <div className="error">Nothing to see here, move along</div>
     }
   }
 
   class App extends React.Component {
     render() {
-      return (<div>
-        <MainHeader title={this.props.title}/>
-        <SplashScreen/>
-        <Navigation/>
-      </div>);
+      return (<HashRouter>
+        <div>
+          <MainHeader title={this.props.title}/>
+          <Switch>
+            <Route exact="exact" path="/" component={SplashScreen}/>
+            <Route exact="exact" path="/seating/" component={Seating}/>
+            <Route component={FouOhFour}/>
+          </Switch>
+          <Navigation/>
+        </div>
+      </HashRouter>);
     }
   }
 
-  ReactDOM.render(
-    <App title="Pick your TV!"/>,
-    document.getElementById('app')
-  );
+  ReactDOM.render(<App title="Pick your TV!"/>, document.getElementById('app'));
 });
